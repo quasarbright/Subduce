8c6b518b83dd1f9e550bfc5cfc35ef794f0dafe4
quasarbright <mthundershock@gmail.com>
2019-07-23 21:10:54 -0400

    input stream

---
+++ language/lexing/lexer.py
@@ -0,0 +1,46 @@
+'''
+The job of the lexer is to tokenize everything
+This doesn't include building parsing trees.
+That is the job of the parser
+'''
+class InputStream:
+    def __init__(self, text):
+        self.text = text
+        self.index = 0
+        self.lineNumber = 1
+        self.characterPosition = 1
+        self.character = None
+        if len(text) > 0:
+            self.character = text[0]
+    def peek(self) -> dict:
+        '''returns state without moving to the next character'''
+        return {
+            "index": self.index,
+            "lineNumber": self.lineNumber,
+            "characterPosition": self.characterPosition,
+            "character": self.character
+        }
+    def advance(self) -> None:
+        '''move to the next character'''
+        self.index += 1
+        self.character = text[index]
+        if character == '\n':
+            self.lineNumber += 1
+            self.characterPosition = 0
+        else:
+            self.characterPosition += 1
+        
+    def getNext(self) -> dict:
+        '''returns state and moves on to next character'''
+        if not self.hasNext():
+            raise RuntimeError('no next')
+        state = self.peek()
+        self.advance()
+        return state
+    def hasNext(self) -> bool:
+        '''does the stream have a next character?'''
+        return self.index < len(self.text) - 1
+
+class Token:
+    def __init__(self):
+        pass
