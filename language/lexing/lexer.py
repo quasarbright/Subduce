6591f19ddcbc87becb9c37a391ec22c9839f1281
quasarbright <mthundershock@gmail.com>
2019-07-25 22:41:26 -0400

    single character tokens, start numbers

--- language/lexing/lexer.py
+++ language/lexing/lexer.py
@@ -4,6 +4,13 @@ This doesn't include building parsing trees.
 That is the job of the parser
 '''
 from typing import List
+import re
+
+specialCharacters = '#()[]:"=\n\t '  # can't be in variables
+oneLengthTokens = '()[]:=\n'  # characters that are tokens of length 1
+keywords = ['def', 'return']
+whitespace = '\t '
+
 class InputStream:
     def __init__(self, text):
         self.text = text
@@ -23,6 +30,8 @@ class InputStream:
         }
     def advance(self) -> None:
         '''move to the next character'''
+        if not self.hasNext():
+            raise RuntimeError('no next')
         self.index += 1
         self.character = text[index]
         if character == '\n':
@@ -30,11 +39,18 @@ class InputStream:
             self.characterPosition = 0
         else:
             self.characterPosition += 1
+    
+    def advanceLine(self) -> None:# needs testing
+        '''move to next line (advances to newline then stops without passing it)'''
+        character = self.peek()['character']
+        while self.hasNext():
+            if character == '\n':
+                return None
+            character = self.getNext()['character']
+        return None
         
     def getNext(self) -> dict:
         '''returns state and moves on to next character'''
-        if not self.hasNext():
-            raise RuntimeError('no next')
         state = self.peek()
         self.advance()
         return state
@@ -43,8 +59,8 @@ class InputStream:
         return self.index < len(self.text) - 1
 
 class Token:
-    def __init__(self, type, lineNumber, startPosition, endPosition, value=None):
-        self.type = type
+    def __init__(self, tokenType, lineNumber, startPosition=None, endPosition=None, value=None):
+        self.type = tokenType
         self.lineNumber = lineNumber
         self.startPosition = startPosition
         self.endPosition = endPosition
@@ -53,5 +69,78 @@ class Token:
         return vars(self) == vars(other)
 
 
+def getTokenType(startCharacter: str) -> str:
+    '''determine token type based on its first character'''
+    assert len(startCharacter) == 1
+    charDict = {
+        '(': '<start function>',
+        ')': '<end function>',
+        '[': '<start list>',
+        ']': '<end list>',
+        ':': '<end signature>',
+        '"': '<string>',
+        '=': '<equals>',
+        '\n': '<newline>',
+    }
+    if startCharacter in charDict:
+        return charDict[startCharacter]
+    else:
+        # it's either an identifier, keyword or a number
+        if startCharacter in '-.0123456789':
+            return '<number>'
+        else:
+            return '<identifier>'
+            # should probably validate that it's what you expect for an identifier and error 
+            # otherwise
+
+def tokenizeNumber(stream: InputStream) -> Token:
+    '''consume number and return token. May raise syntax error
+    alters stream
+    '''
+    state = stream.peek()
+    character = state['character']
+    valueStr = ''
+    while character in '-.0123456789':
+        valueStr += character
+        state = stream.peek()
+        character = state['character']
+        if not stream.hasNext():
+            break
+        stream.advance()
+    ### left off here about to process the number
+    # maybe you want to do this while consuming so you can get character-perfect
+    # syntax error location. But that's messy. I'm thinking just throw it at the
+    # beginning of the number and say syntax error. But that is vague
+        
+        
+
+
 def tokenize(text: str) -> List[Token]:
-    pass
+    stream = InputStream(text)
+    tokens = [Token('<start file>', 1)]
+    while stream.hasNext():
+        state = stream.peek()
+        character = state['character']
+        lineNumber = state['lineNumber']
+        characterPosition = state['characterPosition']
+        # handle whitespace
+        if character in whitespace:
+            # skip whitespace
+            stream.advance()
+            continue
+        elif character == '#':
+            # if comment, go to next line
+            stream.advanceLine()
+        elif character in oneLengthTokens:
+            tokenType = character
+            tokens.append(Token(tokenType, lineNumber, characterPosition, characterPosition+1))
+        else:
+            tokenType = getTokenType(character)
+            if tokenType == '<string>':
+                tokens.append(tokenizeString(stream))
+            elif tokenType == '<number>':
+                tokens.append(tokenizeNumber(stream))
+            elif tokenType == '<identifier>':
+                tokens.append(tokenizeIdentifier(stream))
+        stream.advance()
+    tokens.append('<end file>', lineNumber+1)
