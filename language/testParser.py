4e90b707ea37cefaab8171db1be52d4e73508ea9
quasarbright <mthundershock@gmail.com>
2019-07-30 22:45:15 -0400

    start parser

--- language/testParser.py
+++ language/testParser.py
@@ -6,7 +6,7 @@ from parseTree import *
 identifier = Token(lexer.IDENTIFIER, 1, 1, 2, 'var')
 number = Token(lexer.NUMBER, 1, 1, 2, 123)
 equals = Token(lexer.EQUALS, 1, 1, 2)
-
+string = Token(lexer.STRING, 1, 1, 2, 'hello')
 
 class TestAssignment(unittest.TestCase):
     def testNumberAssignment(self):
@@ -18,3 +18,11 @@ class TestAssignment(unittest.TestCase):
         expected = Assignment(identifier, number)
         actual = parseTokens(tokens)
         self.assertEqual(actual, expected)
+    def testStringAssignment(self):
+        tokens = [
+            identifier,
+            equals,
+            string
+        ]
+        expected = Assignment(identifier, string)
+        actual = parseTokens()
