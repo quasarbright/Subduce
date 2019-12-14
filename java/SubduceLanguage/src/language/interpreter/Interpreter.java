4746b46fa27c349ccbd183130fb6a6a0fd6ab3f4
Mike Delmonaco <mthundershock@gmail.com>
2019-12-13 23:03:40 -0500

    started tests

--- java/SubduceLanguage/src/language/interpreter/Interpreter.java
+++ java/SubduceLanguage/src/language/interpreter/Interpreter.java
@@ -1,9 +1,26 @@
 package language.interpreter;
 
+import java.util.function.Function;
+
+
 public interface Interpreter<ValueType> {
+  /**
+   * Run the program and return an expression evaluator based on the program's definitions.
+   *
+   * @param source the source code to run
+   */
   void run(String source);
 
+  /**
+   * Run the program with an interactive prompt after the code runs.
+   * @param source the source code to run
+   */
   void runWithInteraction(String source);
 
+  /**
+   * Evaluate the expression (using only builtins)
+   * @param expression the expression to evaluate
+   * @return the evaluated expression
+   */
   ValueType eval(String expression);
 }
